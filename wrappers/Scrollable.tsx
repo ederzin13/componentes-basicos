28a0e45dc21a56784756f65307e3ba2e901d49bc
ederzin13 <edermurilo131313@gmail.com>
2026-04-15 22:26:59 -0300

    refresh-control

--- wrappers/Scrollable.tsx
+++ wrappers/Scrollable.tsx
@@ -1,5 +1,12 @@
-import { View, Text, ScrollView, StatusBar, StyleSheet } from "react-native";
-import React from "react";
+import {
+  View,
+  Text,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  RefreshControl,
+} from "react-native";
+import React, { useState } from "react";
 import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
 
 type ScrollableProps = {
@@ -7,10 +14,26 @@ type ScrollableProps = {
 };
 
 export default function Scrollable({ children }: ScrollableProps) {
+  const [refreshing, setRefreshing] = useState<boolean>(false);
+
+  const onRefresh = React.useCallback(() => {
+    setRefreshing(true);
+
+    setTimeout(() => {
+      setRefreshing(false);
+    }, 1000);
+  }, []);
+
   return (
     <SafeAreaProvider>
       <SafeAreaView style={styles.container}>
-        <ScrollView>{children}</ScrollView>
+        <ScrollView
+          refreshControl={
+            <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
+          }
+        >
+          {children}
+        </ScrollView>
       </SafeAreaView>
       <StatusBar barStyle="dark-content"></StatusBar>
     </SafeAreaProvider>
