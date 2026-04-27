95d67b27da7489cc7eedabf478ddb499abb27c0e
ederzin13 <edermurilo131313@gmail.com>
2026-04-26 22:19:14 -0300

    custom button active prop

--- components/Buttons/CustomButton.tsx
+++ components/Buttons/CustomButton.tsx
@@ -1,41 +1,50 @@
 import {
-	Alert,
-	Pressable,
-	StyleSheet,
-	Text,
-	TouchableOpacity,
+  Alert,
+  Pressable,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
 } from "react-native";
 import React from "react";
 import { globalStyles } from "../../styles/globalStyles";
 
 type CustomButtonProps = {
-	text: string;
-	color?: string;
+  text: string;
+  color?: string;
+  active?: boolean;
 };
 
-export default function CustomButton({ text, color }: CustomButtonProps) {
-	return (
-		<TouchableOpacity
-			style={[
-				styles.customButton,
-				{ backgroundColor: color ? color : "#5d513b" },
-			]}
-		>
-			<Pressable
-				onPress={() => Alert.alert("No lugar do Button, use o Pressable!")}
-			>
-				<Text style={globalStyles.whiteText}>{text}</Text>
-			</Pressable>
-		</TouchableOpacity>
-	);
+export default function CustomButton({
+  text,
+  color,
+  active, // = false,
+}: CustomButtonProps) {
+  return (
+    <TouchableOpacity
+      style={[
+        styles.customButton,
+        { backgroundColor: color ? color : "#5d513b" },
+      ]}
+    >
+      <Pressable
+        onPress={() => {
+          if (active) {
+            Alert.alert("No lugar do Button, use o Pressable!");
+          }
+        }}
+      >
+        <Text style={globalStyles.whiteText}>{text}</Text>
+      </Pressable>
+    </TouchableOpacity>
+  );
 }
 
 const styles = StyleSheet.create({
-	customButton: {
-		padding: 15,
-		height: 50,
-		borderRadius: 10,
-		alignItems: "center",
-		justifyContent: "center",
-	},
+  customButton: {
+    padding: 15,
+    height: 50,
+    borderRadius: 10,
+    alignItems: "center",
+    justifyContent: "center",
+  },
 });
--- components/Sections/Buttons.tsx
+++ components/Sections/Buttons.tsx
@@ -6,19 +6,19 @@ import ButtonNativo from "../Buttons/ButtonNativo";
 import CustomButton from "../Buttons/CustomButton";
 
 export default function Buttons() {
-	return (
-		<View style={globalStyles.container}>
-			<TextField>
-				<Text style={globalStyles.caption}>Alguns botões simples</Text>
-				<Text style={globalStyles.paragraph}>
-					É possível utilizar botões nativos (bastante limitados), ou criar
-					botões personalizados utilizando o "Pressable"
-				</Text>
-			</TextField>
-			<ButtonNativo isDisabled={false}></ButtonNativo>
-			<ButtonNativo isDisabled={true}></ButtonNativo>
-			<CustomButton text="Button customizado!"></CustomButton>
-			<CustomButton text="Cor personalizada" color="#676ec3"></CustomButton>
-		</View>
-	);
+  return (
+    <View style={globalStyles.container}>
+      <TextField>
+        <Text style={globalStyles.caption}>Alguns botões simples</Text>
+        <Text style={globalStyles.paragraph}>
+          É possível utilizar botões nativos (bastante limitados), ou criar
+          botões personalizados utilizando o "Pressable"
+        </Text>
+      </TextField>
+      <ButtonNativo isDisabled={false}></ButtonNativo>
+      <ButtonNativo isDisabled={true}></ButtonNativo>
+      <CustomButton text="Button customizado!" active></CustomButton>
+      <CustomButton text="Cor personalizada" color="#676ec3"></CustomButton>
+    </View>
+  );
 }
